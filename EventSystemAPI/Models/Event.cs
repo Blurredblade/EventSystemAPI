0c58fe337c377f50e063a2f57f0480ae4d2b559f
Zachary Cobb <zac.cobb9224@gmail.com>
2019-02-06 14:12:56 -0600

    Added Event Model

---
+++ EventSystemAPI/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventSystemAPI.Models
+{
+    public class Event
+    {
+        public int event_id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string address { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime end_date { get; set; }
+    }
+}
