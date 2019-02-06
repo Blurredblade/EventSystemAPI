e1fd7072c69b8216ce2cce5540fbf67888754131
Zachary Cobb <zac.cobb9224@gmail.com>
2019-02-06 17:48:45 -0600

    Fixed variable names and added comments for event and session models.

--- EventSystemAPI/Models/Event.cs
+++ EventSystemAPI/Models/Event.cs
@@ -5,14 +5,26 @@ using System.Threading.Tasks;
 
 namespace EventSystemAPI.Models
 {
+    /* 
+     * Model for an Event
+     * 
+     * Attributes:
+     * event_id - Unique identifier for each event
+     * address - Address registered for the event instance
+     * start_date -  Starting date set for the event instance
+     * end_date - Ending date set for the event instance
+     * event_name - Name of the event instance
+     * description - Description of the event instance
+     * sessions - A list of all sessions registered to the event instance
+     */
     public class Event
     {
         public int event_id { get; set; }
-        public string name { get; set; }
-        public string description { get; set; }
         public string address { get; set; }
         public DateTime start_date { get; set; }
         public DateTime end_date { get; set; }
+        public string event_name { get; set; }
+        public string description { get; set; }
         public List<Session> sessions { get; set; }
     }
 }
