707e02bdeceebe6217582b4412fb66297fd0c843
Zachary Cobb <zac.cobb9224@gmail.com>
2019-02-06 14:19:46 -0600

    Added Session model

---
+++ EventSystemAPI/Models/Session.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventSystemAPI.Models
+{
+    public class Session
+    {
+        public int session_id { get; set; }
+        public string name { get; set; }
+        public int capacity { get; set; }
+        public int open_slots { get; set; }
+        public DateTime start_end_time { get; set; }
+        public DateTime end_date_time { get; set; }
+    }
+}
