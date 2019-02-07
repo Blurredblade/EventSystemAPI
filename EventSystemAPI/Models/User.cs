6d196c0be8d7391a7b44b6a0a1dbaa9a8e01c61c
Zachary Cobb <zac.cobb9224@gmail.com>
2019-02-06 18:14:53 -0600

    Added Team and User models

---
+++ EventSystemAPI/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventSystemAPI.Models
+{
+    /*
+     * Model for a user
+     * 
+     * Attributes:
+     * user_id - unique identifier for each user
+     * name - the first and last name of the user instance
+     * email - the email of the user instance
+     * phone - the phone number of the user instance
+     * isAdmin - true if the user instance has admin privileges
+     */
+    public class User
+    {
+        public int user_id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public int phone { get; set; }
+        public bool isAdmin { get; set; }
+    }
+}
