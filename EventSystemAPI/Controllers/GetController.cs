f398251a72682df9e56d45ff0f2d8f3a6cdf3d49
Zachary Cobb <zac.cobb9224@gmail.com>
2019-02-14 22:34:34 -0600

    Added new controllers

---
+++ EventSystemAPI/Controllers/GetController.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using EventSystemAPI.Models;
+
+namespace EventSystemAPI.Controllers
+{
+    [Route("esapi/[action]")]
+    [ApiController]
+    public class GetController : ControllerBase
+    {
+        private ESContext db;
+
+        //Constructor that gets called every request
+        public GetController()
+        {
+            db = new ESContext();
+        }
+
+        /*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+         *\\\\\\\\\\\\\\ EVENT METHODS \\\\\\\\\\\\\\\\\
+         *\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+        /* ROUTE esapi/all-events/{user_id}(optional)
+         * GET all events with an optional user id
+         * 
+         * Returns the list of all events in the system
+         * or if the optional user_id is put in it returns 
+         * a list of all events the user is attending
+         */
+        [HttpGet("{user_id:int?}")]
+        [ActionName("all-events")]
+        public ActionResult<List<Event>> GetEventsList(int user_id)
+        {
+            List<Event> events;
+            if (user_id != 0)
+            {
+                events = db.GetEventsList(user_id);
+            }
+            else
+            {
+                events = db.GetEventsList();
+            }
+
+            if (events == null)
+            {
+                return NotFound();
+            }
+            return events;
+        }
+
+        /* ROUTE esapi/event/{event_id}
+        * GET an event by its id
+        */
+        [HttpGet("{event_id:int}")]
+        [ActionName("event")]
+        public ActionResult<Event> GetEvent(int event_id)
+        {
+            Event e = db.GetEvent(event_id);
+            if (e == null)
+            {
+                return NotFound();
+            }
+            return e;
+        }
+
+        /*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+         *\\\\\\\\\\\\\ SESSION METHODS \\\\\\\\\\\\\\\\
+         *\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+        /* ROUTE esapi/event-sessions/{event_id}
+        * GET all the sessions in an event
+        */
+        [HttpGet("{event_id:int}")]
+        [ActionName("event-sessions")]
+        public ActionResult<List<Session>> GetSessionsByEvent(int event_id)
+        {
+            List<Session> sessions = db.GetSessionsByEvent(event_id);
+            if (sessions == null)
+            {
+                return NotFound();
+            }
+            return sessions;
+        }
+
+        /* ROUTE esapi/event-sessions/{session_id}
+         * GET a session by id
+         */
+        [HttpGet("{session_id:int}")]
+        [ActionName("session")]
+        public ActionResult<Session> GetSession(int session_id)
+        {
+            Session session = db.GetSession(session_id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+            return session;
+        }
+
+        /*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+         *\\\\\\\\\\\\\\\ TEAM METHODS \\\\\\\\\\\\\\\\\
+         *\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+        /* ROUTE esapi/team/{team_id}
+         * GET a team by id
+         */
+        [HttpGet("{team_id:int}")]
+        [ActionName("team")]
+        public ActionResult<Team> GetTeam(int team_id)
+        {
+            Team team = db.GetTeam(team_id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            return team;
+        }
+
+        /* ROUTE esapi/event-teams/{event_id}
+         * GET a list of teams for an event
+         */
+        [HttpGet("{event_id:int}")]
+        [ActionName("event-teams")]
+        public ActionResult<List<Team>> GetTeamsByEvent(int event_id)
+        {
+            List<Team> teams = db.GetTeamsByEvent(event_id);
+            if(teams == null)
+            {
+                return NotFound();
+            }
+            return teams;
+        }
+
+        /*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+         *\\\\\\\\\\\ ANNOUNCEMENT METHODS \\\\\\\\\\\\\
+         *\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+        /* ROUTE esapi/event-announcements/{event_id}
+         * GET a list of announcements for an event
+         */
+        [HttpGet("{event_id:int}")]
+        [ActionName("event-announcements")]
+        public ActionResult<List<Announcement>> GetAnnouncementsByEvent(int event_id)
+        {
+            List<Announcement> announcements = db.GetAnnouncementsByEvent(event_id);
+            if(announcements == null)
+            {
+                return NotFound();
+            }
+            return announcements;
+        }
+
+        /* ROUTE esapi/announcement/{announcement_id}
+         * GET an announcement by id
+         */
+        [HttpGet("{announcement_id:int}")]
+        [ActionName("announcement")]
+        public ActionResult<Announcement> GetAnnouncement(int announcement_id)
+        {
+            Announcement announcement = db.GetAnnouncement(announcement_id);
+            if(announcement == null)
+            {
+                return NotFound();
+            }
+            return announcement;
+        }
+
+        /*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+         *\\\\\\\\\\\\\\\ USER METHODS \\\\\\\\\\\\\\\\\
+         *\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+        /* ROUTE esapi/user/{user_id}
+         * GET a user by id
+         */
+        [HttpGet("{user_id:int}")]
+        [ActionName("user")]
+        public ActionResult<User> GetUser(int user_id)
+        {
+            User user = db.GetUser(user_id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return user;
+        }
+
+        /* ROUTE esapi/team-users/{team_id}
+         * GET list of all users on a team
+         */
+        [HttpGet("{team_id:int}")]
+        [ActionName("team-users")]
+        public ActionResult<List<User>> GetTeamUsers(int team_id)
+        {
+            List<User> users = db.GetTeamUsers(team_id);
+            if(users == null)
+            {
+                return NotFound();
+            }
+            return users;
+        }
+
+        /* ROUTE esapi/session-users/{session_id}
+         * GET list of all users attending the given session
+         */
+        [HttpGet("{session_id:int}")]
+        [ActionName("session-users")]
+        public ActionResult<List<User>> GetSessionUsers(int session_id)
+        {
+            List<User> users = db.GetSessionUsers(session_id);
+            if(users == null)
+            {
+                return NotFound();
+            }
+            return users;
+        }
+
+    }
+}
---
+++ EventSystemAPI/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EventSystemAPI.Models;
+
+namespace EventSystemAPI.Controllers
+{
+    [Route("esapi/[action]")]
+    [ApiController]
+    public class POSTController : ControllerBase
+    {
+        private ESContext db;
+
+        //Constructor that gets called every request
+        public POSTController()
+        {
+            db = new ESContext();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+
+
+}
